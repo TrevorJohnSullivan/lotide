8c9e23a0653d1596ffdc716a570d7f597331ac6c
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-07 11:55:17 -0330

    3nd version

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,23 @@
-const assertArraysEqual = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        console.log("this is false");
-      }
-    }
-    console.log("this is true");
+const assertArraysEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("this is false");
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
 
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 5]), true);
