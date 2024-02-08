59ab8ab4fe341aba2188c8be6e5bfc9b3a2e756c
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-08 13:47:16 -0330

    1st version

---
+++ assertObjectsEqual.js
@@ -0,0 +1,45 @@
+const assertObjectsEqual = function (actual, expected) {
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" })
+
+assertObjectsEqual({ c: "1", d: ["2", 3], b : 12 }, { b : 2, d: ["2", 3], c: "1" })
