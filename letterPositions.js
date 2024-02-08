1132491a9dc3f544ed9fd9e0bf91c4fa51e444a4
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-08 10:56:45 -0330

    1st version

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function letterPositions(str) {
+  const obj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      continue;
+    }
+    if (obj[str[i]]) {
+      obj[str[i]].push(i);
+    } else {
+      obj[str[i]] = [];
+      obj[str[i]].push(i);
+    }
+  }
+
+  return obj;
+}
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("lighthouse in the house"))
