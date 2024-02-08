2d9a61ed8063dbb75815eef03b65823b177e9c1c
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-08 13:13:41 -0330

    3rd version

--- eqObjects.js
+++ eqObjects.js
@@ -1,14 +1,13 @@
 const eqArrays = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
+  if (arr1.length !== arr2.length) {
     return false;
   }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const assertEqual = function (actual, expected) {
@@ -19,7 +18,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
@@ -28,13 +27,11 @@ const eqObjects = function (object1, object2) {
   }
 
   for (let key of keys1) {
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-
-  for (let key of keys2) {
-    if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    } else if (object1[key] !== object2[key]) {
       return false;
     }
   }
