134d4eb92ba6f82010b38cf73ba0588782db2534
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-08 11:39:43 -0330

    1st version

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function (object, value) {
+  let tick = -1;
+  for (let i in object) {
+    tick++;
+    if (object[i] === value) {
+      let x = Object.keys(object);
+      return x[tick];
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse"));
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"));
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
