fa494ed0d69a31ddecb77b5e9a8dfc1adc2e2faa
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-09 11:45:51 -0330

    1st version

---
+++ findkey.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const data = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 },
+};
+
+const findKey = function (object, callback) {
+  for (let i in object) {
+    if (callback(object[i])) {
+      return i;
+    }
+  }
+};
+
+const result = findKey(data, (x) => x.stars === 3);
+const expected = "Akaleri";
+
+assertEqual(result, expected);
