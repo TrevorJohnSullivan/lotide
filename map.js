da95a23af4444fd9f04ec8a8afebe6818b62acdd
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-09 09:32:52 -0330

    1st version

---
+++ map.js
@@ -0,0 +1,41 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const actualResults = map(words, (item) => {
+  return item.toUpperCase()
+})
+
+
+const expectedResults = ["GROUND", "CONTROL", "TO", "MAJOR", "TOM"]
+
+assertArraysEqual(actualResults, expectedResults)
