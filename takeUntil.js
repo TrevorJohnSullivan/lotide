baee8feaf2c75b45e2bed49e046f3f0cee627d0e
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-09 10:59:15 -0330

    1st version

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+const takeUntil = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+const actualResults2 = takeUntil(data1, (x) => x < 0);
+const actualResults = takeUntil(data2, (x) => x === ",");
+
+const expectedResults2 = [ 1, 2, 5, 7, 2 ]
+const expectedResults = [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+
+assertArraysEqual(actualResults, expectedResults)
