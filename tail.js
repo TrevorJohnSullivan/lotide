7f4861c4258dc637212b7e68efdb045760813137
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-02-12 10:04:49 -0330

    yo

--- tail.js
+++ tail.js
@@ -1,18 +1,10 @@
+const assertEqual = require('./assertEqual');
+
 const tail = function (arr) {
   let newArr = [];
   newArr = arr.slice(1);
   return newArr;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:  ${actual} !== ${expected}`);
-  }
-};
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); 
-assertEqual(result[0], "Lighthouse"); 
-assertEqual(result[1], "Labs"); 
+module.exports = tail;
